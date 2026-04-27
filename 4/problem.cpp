4941a560e90272a16506e2d713ffa14a06c66e24
Geunho951 <gunho951@gmail.com>
2026-04-27 07:48:47 +0000

    04

--- 4/problem.cpp
+++ 4/problem.cpp
@@ -1,13 +1,45 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
-int main(int argc, char * argv[])
+
+int check_validity(int a, int b)
 {
-	int a,b, sum;
-	a= atoi(argv[1]);
-	b= atoi(argv[2]);
-	if (check_validity(a,b))
-	{
-		sum = sum_up(a,b);
-	}
-	return 0;
+    if (b > a)
+        return 1;
+    else
+        return 0;
+}
+
+int sum_up(int a, int b)
+{
+    int sum = 0;
+
+    for (int i = a; i <= b; i++)
+    {
+        sum += i;
+    }
+
+    cout << "Sum from " << a << " to " << b << " = " << sum << endl;
+
+    return sum;
+}
+
+int main(int argc, char *argv[])
+{
+    int a, b, sum;
+
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+
+    if (check_validity(a, b))
+    {
+        sum = sum_up(a, b);
+    }
+    else
+    {
+        cout << "The second number must be bigger than the first number." << endl;
+        return 0;
+    }
+
+    return 0;
 }
